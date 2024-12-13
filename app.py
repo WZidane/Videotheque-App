2cafc06c4a566fa1fe80d176ce5ef1b4158065c0
WZidane <wail.zidane@hotmail.com>
2024-12-13 16:01:26 +0100

    feat: added register ui

--- app.py
+++ app.py
@@ -2,6 +2,20 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+@app.route('/catalogue', methods=['GET'])
+def catalogue():
+    return render_template('catalogue.html')
+
+@app.route('/register', methods=['GET'])
+def register():
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET'])
+def login():
+    return render_template('login.html')
