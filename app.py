7792e12b300679dfe80a770c0bead4db102e0243
WZidane <wail.zidane@hotmail.com>
2024-12-16 19:36:26 +0100

    feat: added translation

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
-from flask import Flask, render_template, request, jsonify
+import secrets
+from flask import Flask, render_template, request, redirect, session
+from flask_babel import Babel, _
 
 app = Flask(__name__)
 
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+app.secret_key = secrets.token_hex(32)
+
+@app.context_processor
+def inject_get_locale():
+    return dict(get_locale=get_locale)
+
+def get_locale():
+    if request.args.get('lang'):
+        session['lang'] = request.args.get('lang')
+    return session.get('lang', 'fr')
+
+babel = Babel(app, locale_selector=get_locale)
 
 @app.route('/', methods=['GET'])
 def index():
@@ -19,3 +34,8 @@ def register():
 @app.route('/login', methods=['GET'])
 def login():
     return render_template('login.html')
+
+@app.route('/change_language/', methods=['GET'])
+def change_language():
+    session['lang'] = request.args.get('lang')
+    return redirect(request.referrer)
