2da3cad7ef3393a4082ec29496da24d570e9fa2c
Lucas <levojem@gmail.com>
2024-12-17 17:00:35 +0100

    Modifs routes

--- client.py
+++ client.py
@@ -1,22 +1,24 @@
 import requests;
+from dotenv import load_dotenv
+import os
+
+# Load the .env file
+load_dotenv()
 
 class Client:
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {os.getenv('TMDB_KEY')}"
+    }
 
-    def searchMovies(query, page=1):
-        headers = {
-            "accept": "application/json",
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5Yzk3M2M3NTNjMmNlNTFiNjJkN2JiMWY3MTczY2Y4OSIsIm5iZiI6MTczNDEwMDQ1NC4zNjA5OTk4LCJzdWIiOiI2NzVjNDVlNjZhODAwM2I2M2JiN2EyZjkiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.W8NKYICiTLrmn7Hf_IL2wmTkWfAAL6r2FZJcZntrAE4"
-        }
-        response = requests.get('https://api.themoviedb.org/3/search/movie', {'query': query, 'language': 'fr', 'page': page}, headers= headers)
+    @classmethod
+    def searchMovies(cls, query, page=1):
+        response = requests.get('https://api.themoviedb.org/3/search/movie', {'query': query, 'language': 'fr', 'page': page}, headers= cls.headers)
         return response
     
-    def getMovie(id):
+    @classmethod
+    def getMovie(cls, id):
         url = f"https://api.themoviedb.org/3/movie/{id}"
 
-        headers = {
-            "accept": "application/json",
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5Yzk3M2M3NTNjMmNlNTFiNjJkN2JiMWY3MTczY2Y4OSIsIm5iZiI6MTczNDEwMDQ1NC4zNjA5OTk4LCJzdWIiOiI2NzVjNDVlNjZhODAwM2I2M2JiN2EyZjkiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.W8NKYICiTLrmn7Hf_IL2wmTkWfAAL6r2FZJcZntrAE4"
-        }
-
-        response = requests.get(url, {'language': 'fr'}, headers=headers)
+        response = requests.get(url, {'language': 'fr'}, headers=cls.headers)
         return response
