911fb247bb59a03b474cea602a364ba7dae62388
Lucas <levojem@gmail.com>
2024-12-18 18:09:01 +0100

    Feat : get movies by director

--- client.py
+++ client.py
@@ -22,3 +22,36 @@ class Client:
 
         response = requests.get(url, {'language': language}, headers=cls.headers)
         return response
+    
+    @classmethod
+    def getMoviesByDirector(cls, query, page=1, language="fr"):
+        # Resquest to search persons
+        personResponse = requests.get('https://api.themoviedb.org/3/search/person', {'query': query}, headers=cls.headers)
+        data= personResponse.json()
+        directors = []
+        if data.get("total_results") > 0 :
+            for person in data.get("results", []):
+                # Retrieve the ids of only the directors in the list
+                if person.get("known_for_department") == "Directing":
+                    directors.append(person["id"])
+            if(len(directors)> 0):
+                # json where all results will be stored
+                data = []
+                # count of the results
+                total_results = 0
+                for director in directors:
+                    # Request to get the films the director worked on
+                    url = f"https://api.themoviedb.org/3/person/{director}/movie_credits"
+                    response = requests.get(url, {'language': language, 'page': 1}, headers=cls.headers)
+                    dataMovieCredits= response.json()
+                    # Only retrieves the movies where the director worked as a director
+                    for movie in dataMovieCredits.get("crew", []): 
+                        if movie.get("job") == "Director":
+                            data.append(movie)
+                            total_results += 1
+                json_results = {
+                    "total_results" : total_results,
+                    "results" : data
+                }
+                return json_results
+        return None
